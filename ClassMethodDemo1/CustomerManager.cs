b9c70591205f18ccc8a7607f0e5641cb1fc38040
Hilal Oflu <hilal.oflu61@gmail.com>
2021-02-02 01:15:45 +0300

    Add project files.

---
+++ ClassMethodDemo1/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo1
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+
+            Console.WriteLine("Customer " + customer.FirstName + " " + customer.LastName + " WELCOME!");
+        }
+
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine("Customer " + customer.FirstName + " " + customer.LastName + " removed from the system.");
+
+        }
+        public void CustomerBalance(Customer customer)
+        {
+            Console.WriteLine("Customer " + customer.FirstName + " " + customer.LastName + "'s balance has been reorganized.");
+        }
+        public void List(List<Customer> customers)
+        {
+            Console.WriteLine("Customers :");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Id : " + customer.Id + " First Name : " + customer.FirstName + " Last Name : " + customer.LastName + " Balance : " + customer.Balance);
+            }
+        }
+    }
+}
---
+++ ClassMethodDemo1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo1
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Customer customer1 = new Customer();
+                customer1.Id = 61;
+                customer1.FirstName = "Hilal";
+                customer1.LastName = "Oflu";
+                customer1.Balance = 6120;
+
+                Customer customer2 = new Customer();
+                customer2.Id = 35;
+                customer2.FirstName = "Ayşegül";
+                customer2.LastName = "Bingül";
+                customer2.Balance = 1500;
+
+                Customer customer3 = new Customer();
+                customer3.Id = 85;
+                customer3.FirstName = "Beyza";
+                customer3.LastName = "Bodur";
+                customer3.Balance = 8100;
+
+                List<Customer> customerList = new List<Customer> { customer1, customer2, customer3 };
+
+
+                CustomerManager customerManager = new CustomerManager();
+                customerManager.CustomerAdd(customer1);
+                customerManager.CustomerDelete(customer2);
+                customerManager.CustomerBalance(customer3);
+                customerManager.List(customerList);
+
+                Console.ReadLine();
+            }
+        
+    }
+}
